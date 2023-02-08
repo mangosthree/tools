81dde105977a1fa259b185f3f442b61b23982a27
billy1arm <antz@cix.co.uk>
2023-02-08 23:34:13 +0000

    Updated .exe properties

--- EventAICreator/EventAI Creator/Program.cs
+++ EventAICreator/EventAI Creator/Program.cs
@@ -8,10 +8,8 @@ namespace EventAI_Creator
     static class Program
     {
         /// <summary>
-        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// The main entry point for the application.
         /// </summary>
-        
-        
         [STAThread]
         static void Main()
         {
